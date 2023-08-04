df85ab58ad07ef6d5b9a65844ad421ba3415bbe3
David Tello <82037546+DavidTelloMedina@users.noreply.github.com>
2023-08-04 08:04:16 -0500

    CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
 import './App.css'
+import useFetch from './assets/hooks/useFetch'
+import { useEffect, useState } from 'react'
+import UserCard from './components/UserCard'
+import FormUser from './components/FormUser'
 
 function App() {
 
+const [updateInfo, setUpdateInfo] = useState()
+const [closeForm, setCloseForm] = useState(true)
 
+  
+const baseUrl = 'https://users-crud.academlo.tech'
+const [
+  users,
+  getAllUsers,
+  createNewUser,
+  deleteUserById,
+  updateUserById
+] = useFetch(baseUrl,setCloseForm)
+
+useEffect(() => {
+  getAllUsers('/users')
+}, [])
+
+console.log(users)
+const handleOpenForm = () =>{
+setCloseForm(false)
+}
   return (
     
       <div>
-        <h1>Entregable 4</h1>
+        <h1>Users</h1>
+        <button onClick={handleOpenForm} className='formuser__btn'>Open Form</button>
+        <FormUser 
+        createNewUser={createNewUser}
+        updateInfo={updateInfo}
+        updateUserById={updateUserById}
+        setUpdateInfo={setUpdateInfo}
+        closeForm={closeForm}
+        setCloseForm={setCloseForm}
+        handleOpenForm={handleOpenForm}
+        
+
+        />
+        <div>
+          {
+            users?.map(user=>(
+              <UserCard 
+              key={user.id}
+              user={user}
+              deleteUserById={deleteUserById}
+              setUpdateInfo={setUpdateInfo}
+              handleOpenForm={handleOpenForm}
+
+              />
+            ))
+          }
+        </div>
       </div>
         
   )
--- src/assets/hooks/useFetch.js
+++ src/assets/hooks/useFetch.js
@@ -1,14 +1,59 @@
 import { useState } from "react"
 import axios from "axios"
 
-const useFetch = (url) => {
+const useFetch = (baseUrl,setCloseForm) => {
 
     const [infoApi, setInfoApi] = useState()
-    // GET
-    const getApi = ()=>{
-        axios.
+    // GET base URL : users-crud.academlo.tech/
+    const getApi = (path) =>{
+        const url = `${baseUrl}${path}/`
+        axios.get(url)
+         .then(res=>setInfoApi(res.data))
+         .catch(err=>console.log(err))
+         /*
+         fetch(url)
+         .then(res=>res.json())
+         .then(data=>setInfoApi(data))
+         .catch(err=>console.log(err))
+         */
     }
-    return [infoApi,getApi]
+    // POST 
+    const postApi = (path,data)=>{
+        const url=`${baseUrl}${path}/`
+        axios.post(url , data)
+        .then(res=>{
+            console.log(res.data)
+            setInfoApi([...infoApi, res.data])
+            setCloseForm(true)
+        })   
+        .catch(err=>console.log(err))
+    }
+
+    //DELETE
+     const deleteApi = (path, id)=>{
+        const url=`${baseUrl}${path}/${id}/`
+        axios.delete(url)
+        .then(res=>{
+            console.log(res.data)
+            const infoApiFiltered = infoApi.filter(e=>e.id!==id)
+            setInfoApi(infoApiFiltered)
+        })
+        .catch(err=>console.log(err))
+     }
+  //UPDATE
+  const updateApi = (path,id,data)=>{
+    const url=`${baseUrl}${path}/${id}/`
+    axios.patch(url,data)
+    .then(res=>{
+        console.log(res.data)
+        const infoApiMapped =infoApi.map(e=>e.id===id?res.data:e)
+        setInfoApi(infoApiMapped)
+        setCloseForm(true)
+    })
+    .catch(err=>console.log(err))
+  }   
+    
+    return [infoApi,getApi,postApi,deleteApi,updateApi]
 }
 
 export default useFetch
